17955d2f37fc35aff4b4a8f0ebfc51680ab0db68
maxagon <s.shadrov98@gmail.com>
2023-02-04 14:10:21 +0300

    Modulated layer normalization

--- blocks/norm.py
+++ blocks/norm.py
@@ -17,3 +17,34 @@ class BigBatchNorm(nn.Module):
         x = rearrange(x, 'b (c h w) -> b c h w', c=c, h=h, w=w)
         x = x * self.weight.unsqueeze(dim=0).unsqueeze(dim=-1).unsqueeze(dim=-1) + self.bias.unsqueeze(dim=0).unsqueeze(dim=-1).unsqueeze(dim=-1)
         return x
+
+class ModLayerNorm(nn.Module):
+    def __init__(self, num_features, amplitude_mod : nn.Module, affine_mod=True, eps=1e-5):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(1,num_features,1))
+        self.bias = nn.Parameter(torch.zeros(1,num_features,1))
+        self.affine_mod = affine_mod
+        self.eps = eps
+        self.ampl = amplitude_mod
+
+    def forward(self, x, emb=None):
+        _,_,h,w = x.size()
+        x = rearrange(x, 'b c h w -> b c (h w)')
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, keepdim=True)
+        var_sq = (var + self.eps).rsqrt()
+        x = (x - mean) * var_sq
+        amp_in = mean.squeeze(dim=-1)
+        if emb != None:
+            amp_in = torch.cat([amp_in, emb], dim=1)
+        if self.affine_mod:
+            amp_w, amp_b = self.ampl(amp_in).chunk(2, dim=1)
+            amp_w = amp_w.unsqueeze(dim=-1)
+            amp_b = amp_b.unsqueeze(dim=-1)
+            x = x * amp_w + amp_b
+        else:
+            amp_w = self.ampl(amp_in)
+            amp_w = amp_w.unsqueeze(dim=-1)
+            x = x * amp_w
+        x = rearrange(x, 'b c (h w) -> b c h w', h=h, w=w)
+        return x

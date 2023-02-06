e18f21e4a61a57ab3b2422d634f3b56d984c95fd
maxagon <s.shadrov98@gmail.com>
2023-02-06 23:01:37 +0300

    Channelwise noise like sampler

--- loss/augmentation.py
+++ loss/augmentation.py
@@ -1,7 +1,6 @@
 import random
 
 import torch
-import torch.nn as nn
 from einops import rearrange
 
 def rand_spatial_seed():
@@ -11,7 +10,7 @@ def rand_spatial_seed():
         "reverseXY" : random.random() < 0.37
     }
 
-def rand_spatial_apply(tensor, seed = None):
+def rand_spatial_apply(tensor : torch.Tensor, seed = None):
     if seed == None:
         seed = rand_spatial_seed()
     out_t = tensor
@@ -22,3 +21,23 @@ def rand_spatial_apply(tensor, seed = None):
     if seed["reverseXY"]:
         out_t = rearrange(out_t, "b c x y -> b c y x")
     return out_t
+
+@torch.no_grad()
+def channelwise_noise_like(tensor : torch.Tensor):
+    b, s, _, _ = tensor
+    result_rand = []
+    for i in range(b):
+        b_latent = []
+        for si in range(s):
+            target_slice = tensor[i][si]
+            min_lat = torch.min(target_slice)
+            max_lat = torch.max(target_slice)
+            lat_slice_conv = (target_slice - min_lat) / (max_lat - min_lat)
+            mean = torch.mean(lat_slice_conv)
+            std = torch.std(lat_slice_conv)
+            new_lat = torch.normal(mean, std, target_slice.shape).to(tensor.device)
+            new_lat = new_lat * (max_lat - min_lat) + min_lat
+            b_latent.append(new_lat)
+        merged = torch.cat(b_latent, dim=1)
+        result_rand.append(merged)
+    return torch.cat(result_rand, dim=0)

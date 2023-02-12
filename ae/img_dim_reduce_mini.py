1528452f8575b336c78e2b922108d9e796deda57
maxagon <s.shadrov98@gmail.com>
2023-02-12 15:37:56 +0300

    Remove Image Size

--- ae/img_dim_reduce_mini.py
+++ ae/img_dim_reduce_mini.py
@@ -9,15 +9,14 @@ import blocks.nonlinear as nl
 import ae_base as ae
 import blocks.unet as UNet
 
-def ImgDimReduceMini_360k(img_size=256, pretrained=True):
+def ImgDimReduceMini_360k(pretrained=True):
     model = ImgDimReduceMini(
         in_out_dim=3,
         ae_lat_dim=8,
         ae_depth=[1, 1, 2],
         ae_dim=[16, 32, 48],
         unet_depth=[1, 1, 1],
-        unet_dim=[16, 32, 48],
-        img_size=img_size)
+        unet_dim=[16, 32, 48])
     if pretrained:
         model.load_state_dict(torch.load("pretrained/ImgDimReduceMini_360k_laion_aestheticsv2_6.5.bin"))
     return model
@@ -39,35 +38,35 @@ class FactoryAE(UNet.UNetFactory):
     def __init__(self):
         super().__init__()
 
-    def make_downscale(self, img_size, in_dim, out_dim):
+    def make_downscale(self, in_dim, out_dim):
         return spatial.ConvolutionDownscale(in_dim, out_dim)
 
-    def make_upscale(self, img_size, in_dim, out_dim):
+    def make_upscale(self, in_dim, out_dim):
         return spatial.ConvolutionUpscale(in_dim, out_dim)
 
-    def make_feedforward(self, img_size, dim, n_layers):
+    def make_feedforward(self, dim, n_layers):
         return SimpleResBlock(dim=dim, n_layers=n_layers)
 
-    def make_dim_convert(self, img_size, in_dim, out_dim):
+    def make_dim_convert(self, in_dim, out_dim):
         return ff.Conv2DBlock(in_dim=in_dim, out_dim=out_dim, kernel_rad=0, bias=True)
 
 class FactoryUNet(UNet.UNetFactory):
     def __init__(self):
         super().__init__()
 
-    def make_downscale(self, img_size, in_dim, out_dim):
+    def make_downscale(self, in_dim, out_dim):
         return spatial.ConvolutionDownscale(in_dim, out_dim)
 
-    def make_upscale(self, img_size, in_dim, out_dim):
+    def make_upscale(self, in_dim, out_dim):
         return spatial.LanczosUpscale(in_dim, out_dim)
 
-    def make_feedforward(self, img_size, dim, n_layers):
+    def make_feedforward(self, dim, n_layers):
         modules = []
         for i in range(n_layers):
             modules.append(ff.Conv2DBlock(in_dim=dim, out_dim=dim, kernel_rad=1, pad_type='zero', bias=True, nonlinearity=nl.ReLU()))
         return nn.Sequential(*modules)
 
-    def make_dim_convert(self, img_size, in_dim, out_dim):
+    def make_dim_convert(self, in_dim, out_dim):
         return ff.Conv2DBlock(in_dim=in_dim, out_dim=out_dim, kernel_rad=0, bias=False)
 
 class ImgDimReduceMini(ae.AEBase):
@@ -81,12 +80,12 @@ class ImgDimReduceMini(ae.AEBase):
         ae_factory = FactoryAE()
 
         self.rec = UNet.UnetReconstruction(unet_factory, unet_depth, unet_dim)
-        self.down_blocks = ae_factory.unet_downscale_blocks(img_size=img_size, dims=ae_dim, depth=ae_depth)
-        self.up_blocks = ae_factory.unet_upscale_blocks(img_size=img_size, dims=r_ae_dim, depth=r_ae_depth)
-        self.from_img = ae_factory.make_dim_convert(img_size=img_size, in_dim=in_out_dim, out_dim=ae_dim[0])
-        self.to_lat = ae_factory.make_dim_convert(img_size=img_size, in_dim=ae_dim[-1], out_dim=ae_lat_dim)
-        self.from_lat = ae_factory.make_dim_convert(img_size=img_size, in_dim=ae_lat_dim, out_dim=r_ae_dim[0])
-        self.to_img = ae_factory.make_dim_convert(img_size=img_size, in_dim=r_ae_dim[-1], out_dim=in_out_dim)
+        self.down_blocks = ae_factory.unet_downscale_blocks(dims=ae_dim, depth=ae_depth)
+        self.up_blocks = ae_factory.unet_upscale_blocks(dims=r_ae_dim, depth=r_ae_depth)
+        self.from_img = ae_factory.make_dim_convert(in_dim=in_out_dim, out_dim=ae_dim[0])
+        self.to_lat = ae_factory.make_dim_convert(in_dim=ae_dim[-1], out_dim=ae_lat_dim)
+        self.from_lat = ae_factory.make_dim_convert(in_dim=ae_lat_dim, out_dim=r_ae_dim[0])
+        self.to_img = ae_factory.make_dim_convert(in_dim=r_ae_dim[-1], out_dim=in_out_dim)
 
     def encode_features(self, input):
         out = input
--- blocks/unet.py
+++ blocks/unet.py
@@ -8,69 +8,65 @@ class UNetFactory():
         pass
 
     @abstractmethod
-    def make_downscale(self, img_size, in_dim, out_dim):
+    def make_downscale(self, in_dim, out_dim):
         pass
 
     @abstractmethod
-    def make_upscale(self, img_size, in_dim, out_dim):
+    def make_upscale(self, in_dim, out_dim):
         pass
 
     @abstractmethod
-    def make_feedforward(self, img_size, dim, n_layers):
+    def make_feedforward(self, dim, n_layers):
         pass
 
     @abstractmethod
-    def make_dim_convert(self, img_size, in_dim, out_dim):
+    def make_dim_convert(self, in_dim, out_dim):
         pass
 
     # apply downscale dims-1 times
     # dims = 1 -> [[ResBlock]]
     # dims = 2 -> [[ResBlock], [Downscale, ResBlock]]
-    def unet_downscale_blocks(self, img_size, dims, depth):
+    def unet_downscale_blocks(self, dims, depth):
         modules = []
         assert(len(dims) == len(depth))
         cur_dim = dims[0]
-        cur_img_size = img_size
         if depth[0] != 0:
-            modules.append(self.make_feedforward(img_size=img_size, dim=dims[0], n_layers=depth[0]))
+            modules.append(self.make_feedforward(dim=dims[0], n_layers=depth[0]))
         for i in range(1, len(dims)):
             next_dim = dims[i]
             blocks = []
-            blocks.append(self.make_downscale(img_size=cur_img_size, in_dim=cur_dim, out_dim=next_dim))
-            cur_img_size = img_size // 2
+            blocks.append(self.make_downscale(in_dim=cur_dim, out_dim=next_dim))
             cur_dim = next_dim
-            blocks.append(self.make_feedforward(img_size=cur_img_size, dim=cur_dim, n_layers=depth[i]))
+            blocks.append(self.make_feedforward(dim=cur_dim, n_layers=depth[i]))
             modules.append(nn.Sequential(*blocks))
         return nn.ModuleList(modules)
     # apply upscale dims-1 times
     # dims = 1 -> [[ResBlock]]
     # dims = 2 -> [[ResBlock], [Upscale, ResBlock]]
-    def unet_upscale_blocks(self, img_size, dims, depth):
+    def unet_upscale_blocks(self, dims, depth):
         modules = []
         assert(len(dims) == len(depth))
         cur_dim = dims[0]
-        cur_img_size = img_size
         if depth[0] != 0:
-            modules.append(self.make_feedforward(img_size=img_size, dim=dims[0], n_layers=depth[0]))
+            modules.append(self.make_feedforward(dim=dims[0], n_layers=depth[0]))
         for i in range(1, len(dims)):
             next_dim = dims[i]
             blocks = []
-            blocks.append(self.make_upscale(img_size=cur_img_size, in_dim=cur_dim, out_dim=next_dim))
-            cur_img_size = img_size // 2
+            blocks.append(self.make_upscale(in_dim=cur_dim, out_dim=next_dim))
             cur_dim = next_dim
-            blocks.append(self.make_feedforward(img_size=cur_img_size, dim=cur_dim, n_layers=depth[i]))
+            blocks.append(self.make_feedforward(dim=cur_dim, n_layers=depth[i]))
             modules.append(nn.Sequential(*blocks))
         return nn.ModuleList(modules)
     # [ResBlock(dim*2), DimConvert(dim*2->dim), ResBlock(dim)]
-    def unet_cat_block(self, img_size, dim, depth):
+    def unet_cat_block(self, dim, depth):
         modules = []
-        modules.append(self.make_feedforward(img_size=img_size, dim=dim*2, n_layers=depth))
-        modules.append(self.make_dim_convert(img_size=img_size, in_dim=dim*2, out_dim=dim))
-        modules.append(self.make_feedforward(img_size=img_size, dim=dim, n_layers=depth))
+        modules.append(self.make_feedforward(dim=dim*2, n_layers=depth))
+        modules.append(self.make_dim_convert(in_dim=dim*2, out_dim=dim))
+        modules.append(self.make_feedforward(dim=dim, n_layers=depth))
         return nn.Sequential(*modules)
 
 class UnetReconstruction(nn.Module):
-    def __init__(self, factory : UNetFactory, depth, dim, img_size=256):
+    def __init__(self, factory : UNetFactory, depth, dim):
         super().__init__()
 
         self.depth = len(depth)
@@ -79,14 +75,12 @@ class UnetReconstruction(nn.Module):
         r_depth = list(reversed(depth))
         r_dim = list(reversed(dim))
 
-        self.down_blocks = factory.unet_downscale_blocks(img_size=img_size, dims=dim, depth=depth)
-        self.up_blocks = factory.unet_upscale_blocks(img_size=16, dims=r_dim, depth=r_depth)
+        self.down_blocks = factory.unet_downscale_blocks(dims=dim, depth=depth)
+        self.up_blocks = factory.unet_upscale_blocks(dims=r_dim, depth=r_depth)
 
         cat_blocks = []
-        cur_img_size = img_size
         for i in range(self.depth):
-            cat_blocks.append(factory.unet_cat_block(img_size=cur_img_size, dim=dim[i], depth=depth[i]))
-            cur_img_size = cur_img_size // 2
+            cat_blocks.append(factory.unet_cat_block(dim=dim[i], depth=depth[i]))
         cat_blocks.reverse()
         self.cat_blocks = nn.ModuleList(cat_blocks)
 

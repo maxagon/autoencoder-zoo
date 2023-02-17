090194e0979d54658a89e82549676e647dd41a04
maxagon <s.shadrov98@gmail.com>
2023-02-17 22:21:16 +0300

    Fix zero std

--- loss/augmentation.py
+++ loss/augmentation.py
@@ -31,7 +31,7 @@ def rand_spatial_apply(tensor : torch.Tensor, seed = None):
 
 @torch.no_grad()
 def channelwise_noise_like(tensor : torch.Tensor):
-    b, s, _, _ = tensor
+    b, s, _, _ = tensor.shape
     result_rand = []
     for i in range(b):
         b_latent = []
@@ -39,12 +39,15 @@ def channelwise_noise_like(tensor : torch.Tensor):
             target_slice = tensor[i][si]
             min_lat = torch.min(target_slice)
             max_lat = torch.max(target_slice)
-            lat_slice_conv = (target_slice - min_lat) / (max_lat - min_lat)
+            lat_slice_conv = (target_slice - min_lat) / (max_lat - min_lat + 0.00001)
             mean = torch.mean(lat_slice_conv)
             std = torch.std(lat_slice_conv)
-            new_lat = torch.normal(mean, std, target_slice.shape).to(tensor.device)
-            new_lat = new_lat * (max_lat - min_lat) + min_lat
-            b_latent.append(new_lat)
+            if std == 0 or torch.isnan(std):
+                new_lat = torch.full_like(target_slice, mean)
+            else:
+                new_lat = torch.normal(mean, std, target_slice.shape).to(tensor.device)
+                new_lat = new_lat * (max_lat - min_lat) + min_lat
+            b_latent.append(new_lat.unsqueeze(0).unsqueeze(0))
         merged = torch.cat(b_latent, dim=1)
         result_rand.append(merged)
     return torch.cat(result_rand, dim=0)

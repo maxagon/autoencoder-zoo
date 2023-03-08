6320ea87c24fb0825e519a7508ddcb64ce8fbc5f
maxagon <s.shadrov98@gmail.com>
2023-03-08 13:37:03 +0300

    Gaussian dropout scheduler

--- blocks/gauss_vae.py
+++ blocks/gauss_vae.py
@@ -3,15 +3,44 @@ import math
 import torch
 import torch.nn as nn
 
+class GaussianDropoutScheduler():
+    def __init__(self, start_dropout = 0.2, end_dropount = 0.0, warmup_steps = 20000, exp_decay = 10000):
+        self.blocks = []
+        self.counter = 0
+        self.start_dropout = start_dropout
+        self.end_dropount = end_dropount
+        self.warmup_steps = warmup_steps
+        self.exp_decay = exp_decay
+
+    def make(self):
+        dropout_block = GaussianDropout(dropout=self.start_dropout)
+        self.blocks.append(dropout_block)
+        return dropout_block
+
+    def step(self):
+        self.counter = self.counter + 1
+        if self.counter > self.warmup_steps:
+            step = self.counter - self.warmup_steps
+            current_dropout = self.end_dropout + (self.start_dropout - self.end_dropout) * math.exp(-step / self.exp_decay)
+            self.update(current_dropout)
+
+    def update(self, dropout):
+        for b in self.blocks:
+            b.set_dropout(dropout)
+
 # Gaussian dropout as an information bottleneck layer
 # http://bayesiandeeplearning.org/2021/papers/40.pdf
 class GaussianDropout(nn.Module):
     def __init__(self, dropout=0.1):
         super().__init__()
+        self.set_dropout(dropout=dropout)
+
+    def set_dropout(self, dropout):
         self.std = math.sqrt(dropout / (1.0 - dropout))
+
     def forward(self, x):
         out = x
-        noise = torch.normal(mean=1.0, std=self.std, size=x.shape).to(out.device)
+        noise = torch.normal(mean=1.0, std=self.std, size=x.shape, device=out.device)
         out = out * noise
         return out
 

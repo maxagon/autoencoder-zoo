698eb5c3cdee0b729586bb487476130b7bd07fd1
maxagon <s.shadrov98@gmail.com>
2023-03-17 23:16:49 +0300

    Ternary loss

---
+++ loss/ternary.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class Ternary(nn.Module):
+    def __init__(self, patch_size=7):
+        super(Ternary, self).__init__()
+        self.patch_size = patch_size
+        out_channels = patch_size * patch_size
+        w = np.eye(out_channels).reshape((patch_size, patch_size, 1, out_channels))
+        w = np.transpose(w, (3, 2, 0, 1))
+        self.register_buffer("w", torch.tensor(w).float())
+
+    def transform(self, tensor):
+        tensor_ = tensor.mean(dim=1, keepdim=True)
+        patches = F.conv2d(tensor_, self.w, padding=self.patch_size//2, bias=None)
+        loc_diff = patches - tensor_
+        loc_diff_norm = loc_diff / torch.sqrt(0.81 + loc_diff ** 2)
+        return loc_diff_norm
+
+    def valid_mask(self, tensor):
+        padding = self.patch_size//2
+        b, c, h, w = tensor.size()
+        inner = torch.ones(b, 1, h - 2 * padding, w - 2 * padding).type_as(tensor)
+        mask = F.pad(inner, [padding] * 4)
+        return mask
+        
+    def forward(self, x, y):
+        loc_diff_x = self.transform(x)
+        loc_diff_y = self.transform(y)
+        diff = loc_diff_x - loc_diff_y.detach()
+        dist = (diff ** 2 / (0.1 + diff ** 2)).mean(dim=1, keepdim=True)
+        mask = self.valid_mask(x)
+        loss = (dist * mask).mean()
+        return loss
